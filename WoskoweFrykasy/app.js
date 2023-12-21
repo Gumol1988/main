1117902de9228b6e5d85eaa4977ca7735c13a070
Gumol1988 <153996298+Gumol1988@users.noreply.github.com>
2023-12-21 11:44:40 +0100

    Add files via upload

--- WoskoweFrykasy/app.js
+++ WoskoweFrykasy/app.js
@@ -1,6 +1,19 @@
 const hamburger = document.querySelector('.hamburger')
 const mobileMenu = document.querySelector('.navLinks')
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.nav');
+  
+    window.addEventListener('scroll', function() {
+      if (window.scrollY > navbar.offsetHeight) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    });
+  });
+
 hamburger.addEventListener('click',()=> {
     hamburger.classList.toggle('active')
     mobileMenu.classList.toggle('active')
