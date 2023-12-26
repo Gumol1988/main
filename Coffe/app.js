69c1bc7b0d3899fe5b6e08a88fc4920f1cf5f37d
Gumol1988 <153996298+Gumol1988@users.noreply.github.com>
2023-12-26 14:26:41 +0100

    Add files via upload

--- Coffe/app.js
+++ Coffe/app.js
@@ -2,16 +2,30 @@ const btnLoop = document.querySelector('.bx-search')
 const searchInput = document.querySelector('#searchBox')
 const hamburger = document.querySelector('.bx-menu')
 const nav = document.querySelector('.nav')
+const cart = document.querySelector('.cartContainer')
+const cartIcon = document.querySelector('.bx-cart')
+
+
+
+const changeActiveCart = () => {
+	cart.classList.toggle('active')	
+	nav.classList.remove('active')
+	searchInput.classList.remove('active')
+}
 
 const changeActiveSearch = () => {
 	searchInput.classList.toggle('active')
 	nav.classList.remove('active')
+	cart.classList.remove('active')
 }
 
 const changeActiveMobile = params => {
 	nav.classList.toggle('active')
 	searchInput.classList.remove('active')
+	cart.classList.remove('active')
 }
 
+
+cartIcon.addEventListener('click', changeActiveCart)
 hamburger.addEventListener('click', changeActiveMobile)
 btnLoop.addEventListener('click', changeActiveSearch)
